3f9eb5326ac23d69c3cc65001b13e126edc90ed6
aenzo <enzo.achard@ynov.com>
2023-04-04 11:21:40 +0200

    Fight WIP

---
+++ main.js
@@ -0,0 +1,46 @@
+class ship {
+    constructor() {
+        this.name = 'ship';
+        this.hpMax = 100;
+        this.hp = 100;
+        // this.attack = Math.floor(Math.random() * 21) + 5;
+        this.defense = Number
+    }
+    attack(target) {
+        const attack = Math.floor(Math.random() * 21) + 5;
+        target.hp -= attack
+    }
+    
+}
+const playerShip = new ship();
+const hostileShip = new ship();
+fight()
+
+
+
+function fight(){
+    playerShip.attack(hostileShip);
+    hostileShip.attack(playerShip);
+}
+
+// playerShip.attack(hostileShip);
+// hostileShip.attack(playerShip);
+
+console.log(`HP hostileShip : ${hostileShip.hp}`)
+console.log(`HP playerShip : ${playerShip.hp}`)
+sleep(4000)
+
+
+
+
+// console.log(playerShip.attack);
+// console.log(hostileShip.attack);
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+  
