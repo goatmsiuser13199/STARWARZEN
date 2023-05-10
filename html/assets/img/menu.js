5d334320a87a9613ec71a32a0d887ae2d6bcdcc9
loretban <esteban.loret@ynov.com>
2023-05-10 18:40:13 +0200

    Dialog mise a jour

---
+++ html/assets/img/menu.js
@@ -0,0 +1,11 @@
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
+
+toggle.addEventListener('click', function() {
+    if(body.classList.contains('open')) {
+        body.classList.remove('open');
+    }
+    else {
+        body.classList.add('open');
+    }
+})
